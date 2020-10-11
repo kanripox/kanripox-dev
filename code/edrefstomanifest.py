93d0242547b8a242155a145cfa39ffcf2965e784
ChristianW <cwittern@yahoo.com>
2020-10-11 14:27:09 +0900

    Code to create and expand manifest.

---
+++ code/edrefstomanifest.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import xml.etree.ElementTree as ET
+import os
+import linktables
+
+krx_xmlns="{http://kanripo.org/ns/KRX/Manifest/1.0}"
+ET.register_namespace('',"http://kanripo.org/ns/KRX/Manifest/1.0")
+textdir="."
+segs = linktables.read_align_tabs(textdir)
+
+tokd="%s/aux/tok"%(textdir)
+#os.chdir(textdir)
+
+mtree=ET.parse("Manifest.xml")
+parent_map = {c: p for p in mtree.iter() for c in p}
+edrefs = mtree.findall(f'.//{krx_xmlns}edRef')
+
+tks = {}
+for edref in edrefs:
+    div = parent_map[edref]
+    txtid = edref.attrib['key']
+    if not txtid in tks:
+        tkfiles = [f for f in os.listdir(tokd) if f.startswith(txtid)]
+        tkfiles.sort()
+        tx = {}
+        for f in tkfiles:
+            tktree = ET.parse("%s/%s" % (tokd, f))
+            for t in tktree.findall('.//t'):
+                tp = t.attrib['tp']
+                idx = t.attrib['id']
+                tx[tp] = idx
+        tks[txtid] = tx
+
+    start = edref.attrib['start']
+    end = edref.attrib['end']
+    elid = tks[txtid][start]
+    endid = tks[txtid][end]
+    for el in segs[elid]:
+        if el[0] == txtid:
+            continue
+        nstart = int(el[2]) + 1
+        key = el[0]
+        ex = [a for a in segs[endid] if a[0] == key]
+        if len (ex) > 0:
+            nend = int(ex[0][2]) + 1
+            if (nend > nstart):
+                new = ET.SubElement(div, f'{krx_xmlns}edRef')
+                new.attrib['key'] = key
+                new.attrib['start'] = str(nstart)
+                new.attrib['end'] = str(nend)
+            print (nend)
+
+mtree.write("Manifest-n.xml", encoding="utf-8", xml_declaration=True, method="xml", short_empty_elements=True)
---
+++ code/makemanifest.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+import xml.etree.ElementTree as ET
+
+tei="{http://www.tei-c.org/ns/1.0}"
+krx="{http://kanripo.org/ns/KRX/Manifest/1.0}"
+stub="""<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns="http://kanripo.org/ns/KRX/Manifest/1.0">
+  <description></description>
+  <editions>
+  %s
+  </editions>
+</manifest>
+"""
+
+ed_tpl =f"""<edition id="%s" format="%s" location="%s" type="%s" language="lzh">
+      <description>%s</description>
+</edition>
+"""
+tpx="doc"
+d={"doc": "documentary", "int": "interpretative"}
+col=[]
+for tpx in ["doc", "int"]:
+    eds = [a for a in os.listdir(tpx)]
+    eds.sort()
+    doc=[]
+    for ed in eds:
+        p=f"{tpx}/{ed}"
+        f=[a for a in os.listdir(p) if a.endswith("xml")]
+        if len(f) > 0:
+            f=[a for a in os.listdir(p) if a.endswith("xml") and not ("_" in a)][0]
+            tree = ET.parse(f"{p}/{f}")
+            root = tree.getroot()
+            titx = root.findall(f'.//{tei}title')
+            if len(titx) > 0:
+                tit = titx[0].text
+            else:
+                tit = ""
+            fm="xml/TEI"
+            tp=d[tpx]
+        else:
+            f1=[a for a in os.listdir(p) if a.endswith("txt")]
+            r = [a for a in open(f"{p}/{f1[0]}").readlines() if a.startswith("#+TITLE:")][0]
+            tit = r[:-1].split()[-1]
+            fm="txt/mandoku"
+            tp=d[tpx]
+        doc.append(ed_tpl % (ed, fm, p, tp, tit))
+    dx="".join(doc)
+    # col.append (f"<{d[tpx]}>\n{dx}</{d[tpx]}>")
+    col.append (f"{dx}")
+of=open("Manifest.xml", mode="w", encoding="utf8")
+of.write(stub % ("\n".join(col)))
+of.close()
